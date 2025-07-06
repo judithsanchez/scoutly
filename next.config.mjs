abb4b2c7978cd375e069f8a7a96528413c6bba60
judithsanchez <judithv.sanchezc@gmail.com>
2025-07-06 16:01:14 +0200

    feat: Update CORS policy to restrict Access-Control-Allow-Origin to specific domain

--- next.config.mjs
+++ next.config.mjs
@@ -35,7 +35,10 @@ const nextConfig = {
 				source: '/api/:path*',
 				headers: [
 					{key: 'Access-Control-Allow-Credentials', value: 'true'},
-					{key: 'Access-Control-Allow-Origin', value: '*'},
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: 'https://www.jobscoutly.tech',
+					},
 					{
 						key: 'Access-Control-Allow-Methods',
 						value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
