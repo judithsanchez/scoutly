72c942e48d5fc84a1655f53657561bcfbee3e757
judithsanchez <judithv.sanchezc@gmail.com>
2025-07-11 22:59:39 +0200

    refactor(userService): ensure database connection is established in all user service methods

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,21 +1,26 @@
+import {connectDB} from '@/config/database';
 import {User} from '@/models/User';
 
 export class UserService {
 	static async createUser(data: Record<string, any>) {
+		await connectDB();
 		const user = new User(data);
 		await user.save();
 		return user;
 	}
 
 	static async getAllUsers() {
+		await connectDB();
 		return User.find({});
 	}
 
 	static async getUserByEmail(email: string) {
+		await connectDB();
 		return User.findOne({email});
 	}
 
 	static async promoteUser(data: {email: string}, secret?: string) {
+		await connectDB();
 		if (!data?.email) {
 			throw new Error('Email is required');
 		}
